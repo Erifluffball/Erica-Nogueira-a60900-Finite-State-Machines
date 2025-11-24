4cd5a9db5a14a20a13da4af95fe2820c35f2bec9
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-11-24 00:18:50 +0000

    Chasing the Player Part1 End

--- Assets/State.cs
+++ Assets/State.cs
@@ -194,3 +194,32 @@ public class Pursue : State
         base.Exit();
     }
 }
+
+public class Attack : State 
+{
+    float rotationSpeed = 2.0f;
+    AudioSource shoot;
+    public Attack(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.ATTACK;
+        shoot = npc.GetComponent<AudioSource>();
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isShooting");
+        agent.isStopped = true;
+        shoot.Play();
+        base.Enter();
+    }
+
+    public override void Update()
+    { 
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
