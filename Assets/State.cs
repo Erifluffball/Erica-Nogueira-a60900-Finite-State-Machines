1cb7aeec80eeccc936933bb7b49beb6eed43cb7d
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-11-24 09:28:23 +0000

    Added RunAway state

--- Assets/State.cs
+++ Assets/State.cs
@@ -273,3 +273,37 @@ public class Attack : State
         base.Exit();
     }
 }
+
+public class RunAway : State
+{
+    GameObject safeLocation;
+    public RunAway(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.RUNAWAY;
+        safeLocation = GameObject.FindGameObjectWithTag("Safe");
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isRunning");
+        agent.isStopped = false;
+        agent.speed = 6;
+        agent.SetDestination(safeLocation.transform.position);
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            nextState = new Idle(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+    }  
+   
+    public override void Exit()
+    {
+        anim.ResetTrigger("isRunning");
+        base.Exit();
+    }
+}
